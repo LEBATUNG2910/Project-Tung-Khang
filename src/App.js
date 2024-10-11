831862222aefcd2af03229f769ad227d9c32ac14
AndrewKhang <dunglammattaikhoan@gmail.com>
2024-10-11 09:18:35 +0700

    Project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // Import 'Link'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import 'Link'
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Login from "./pages/Login";
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -68,12 +68,12 @@ const Login = () => {
                 </div>
 
                 <div className="text-sm">
-                  <a
+                  {/* <a
                     className="font-medium text-indigo-500 hover:text-indigo-400"
                     href="#"
                   >
                     Forgot your password?
-                  </a>
+                  </a> */}
                 </div>
               </div>
 
@@ -89,12 +89,12 @@ const Login = () => {
           </div>
           <div className="px-8 py-4 bg-gray-700 text-center">
             <span className="text-gray-400">Don't have an account?</span>
-            <a
+            {/* <a
               className="font-medium text-indigo-500 hover:text-indigo-400"
               href="#"
             >
               Sign up
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
